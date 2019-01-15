670350bd60ec6605a4b41e6c5d4689bf929d8eb9
Zajic Pavel <zajic@frisoft.cz>
2019-01-15 23:05:44 +0100

    Doplnění stránek (pages)

---
+++ KB-AspNetCore-Razor-EFCore-InMemory/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using KB_AspNetCore_Razor_EFCore_InMemory.Model;
+
+namespace KB_AspNetCore_Razor_EFCore_InMemory.Pages
+{
+    public class IndexModel : PageModel
+    {
+      private readonly InMemoryDbContext _context;
+
+      public IndexModel(InMemoryDbContext context)
+      {
+        _context = context;
+      }
+
+      public EntFyzOsoba entFyzOsoba{ get; set; }
+
+      public async Task OnGetAsync()
+      {
+        entFyzOsoba = _context.EntFyzOsoba.FirstOrDefault<EntFyzOsoba>(x => x.ID == "20190114222300000001");
+      }
+    }
+}
